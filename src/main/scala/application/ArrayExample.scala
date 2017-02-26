548fdb7a45d5cde1befb5dcb2ec417a6ceda674d
Mark Kewley <markkewley@Mark-Kewleys-iMac.local>
2017-02-26 09:44:29 -0600

    Adding in Array example, implementation to come

---
+++ src/main/scala/application/ArrayExample.scala
@@ -0,0 +1,14 @@
+package application
+
+/**
+  * Scala's Array is the same as Java's []. For example
+  * Array[Int] is the same as int[].
+  */
+object ArrayExample extends CollectionsExample {
+
+
+
+  override def runExample(): Unit = {
+
+  }
+}
